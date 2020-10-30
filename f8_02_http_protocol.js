05f087e81a1eb70538ce3ccca09f09f5d03a914a
Nam Tran <nam.tran@kyanon.digital>
2020-10-30 10:17:08 +0700

    Upload Source Version 1

---
+++ f8_02_http_protocol.js
@@ -0,0 +1,3 @@
+// HyperText Transfer Protocol : Giao thức truyển tải siêu văn bản, là một trong năm giao thức chuẩn của mạng Internet.
+// Chi tiết xem ở wiki HTTP : https://vi.wikipedia.org/wiki/Hypertext_Transfer_Protocol
+
---
+++ f8_03_ssr_csr.js
@@ -0,0 +1,3 @@
+// 1. Server side rendering
+// 2. Client side rendering
+// Tìm hiểu ở : https://viblo.asia/p/server-side-rendering-va-client-side-rendering-yMnKMw2El7P
